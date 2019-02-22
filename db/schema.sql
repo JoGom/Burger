ae1303eddf2c8bd6fd55413fd136a54c6de6fdfe
Jorge G <jogoram428@gmail.com>
2019-02-22 12:36:52 -0700

    created connection to datavase and started ORM file

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,11 @@
+DROP DATABASE IF EXISTS burgers_db;
 CREATE DATABASE burgers_db;
 
-USE DATABASE burgers_db;
+USE burgers_db;
 
 CREATE TABLE burgers(
-    id INTEGER NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT,
     burger_name VARCHAR(100) NOT NULL,
-    devoured BOOLEAN NOT NULL DEFAULT 0
+    devoured BOOLEAN DEFAULT false,
+    PRIMARY KEY (id)
 );
