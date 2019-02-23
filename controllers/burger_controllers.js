8b0e8893982dbf1db2d387300c15e256bc703052
Jorge G <jogoram428@gmail.com>
2019-02-22 23:41:13 -0700

    created routes to create and update

--- controllers/burger_controllers.js
+++ controllers/burger_controllers.js
@@ -9,8 +9,27 @@ router.get("/", function(req, res) {
 
 router.get("/home", function(req, res) {
     burger.selectAll(function(data) {
-        res.json(data);
-    })
-})
+        res.render("index", {burgers: data});
+    });
+});
+
+router.post("/home/create", function(req, res) {
+    burger.insertOne(req.body.name, function(data) {
+        console.log(data);
+        console.log("Burger Added");
+        //refreshes the page to show new burger
+        res.redirect("/home");
+    });
+});
+
+router.put("/home/devour/:id", function(req, res) {
+    burger.updateOne(req.params.id, function(result) {
+    console.log(result)
+    console.log(`burger ${req.params.id} eaten!`);
+    res.status(200).end();
+    });
+});
+
+
 
 module.exports = router;
